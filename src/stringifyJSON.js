672872e39cfd07906f665ac17ad2f5c1745b5c4f
macqueen <jessmacqueen@gmail.com>
2013-07-31 16:47:56 -0700

    deleted comments, fixed bracket slicing issue

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,40 +3,6 @@
 
 // but you don't so you're going to have to write it from scratch:
 
-/*
-var stringifyJSON = function (obj) {
-  var resultString = '';
-  var updaterString = '';
-  for (prop in obj) {
-  	// string, bool or integer
-  	if (typeof obj === 'string' || typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'null') {
-  		updaterString = '\"' + prop + '\"' + ':' + '\"' + String(obj) + '\"';
-  		resultString = resultString.concat(updaterString) + ',';
-  	}
-  	// array
-  	else if (Object.prototype.toString.call(obj) === '[object Array]') {
-  		updaterString = '\"' + prop + '\"' + ':' + '[';
-  		resultString = resultString.concat(updaterString);
-  		for (var i = 0; i < obj.length; i++) {
-  			resultString = resultString.concat(String(obj[i]));
-  		}
-  		resultString = resultString + '],'
-  	}
-  	// object
-  	else if (Object.prototype.toString.call(obj) === '[object Object]') {
-  		updaterString = stringifyJSON(obj);
-  		resultString = resultString.concat(updaterString);
-  	}
-  }
-  if (resultString[resultString.length - 1] === ',') {
-  	resultString = resultString.slice(0, -1);
-  }
-  resultString = '{' + resultString + '}';
-  //console.log(resultString);
-  return resultString;
-};
-
-*/
 
 var stringifyJSON = function (obj) {
   var resultString = '';
@@ -83,18 +49,11 @@ var stringifyJSON = function (obj) {
   		updaterString = stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
   		resultString = resultString.concat(updaterString) + ',';
   	}
-  	resultString = resultString.slice(0, -1) + '}';
-  	//resultString = resultString + '}';
+  	if (resultString[resultString.length - 1] === ',') {
+  		resultString = resultString.slice(0, -1);
+  	}
+  	resultString = resultString + '}';
   	return resultString;
-  	/*updaterString = stringifyJSON(obj);
-  	resultString = resultString.concat(updaterString);
-  	return resultString; */
   }
-
-  //if (resultString[resultString.length - 1] === ',') {
-  	//resultString = resultString.slice(0, -1);
-  //}
-  // resultString = '{' + resultString + '}';
-  // console.log(resultString);
   return resultString;
 };
