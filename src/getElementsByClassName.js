0a6655d154de8cae0b6922800233f46e48749e93
macqueen <jessmacqueen@gmail.com>
2013-08-02 19:43:12 -0700

    restructured function to prevent infinite loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,63 +4,30 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  var elem = document.body;
-  var matchElems = [];
+var getElementsByClassName = function (className, currentNode, matches) {
+  var elem = currentNode || document.body;
+  var matchElems = matchElems || [];
 
-  if (elem.childNodes.length !== 0) {
-  	var child = elem.firstChild;
-  	// console.log(child);
-  	while(child) {
-  		if (child.nodeType === 1) {
-  			if (child.classList.contains(className)) {
-  				matchElems.push(child);
-  			}
-  			child.getElementsByClassName(className);
-  			child = child.nextSibling;
-  		}
-  		else {
-  			child = child.nextSibling;
-  		}
-  		console.log(child);
+  if (elem.nodeType === 1) {
+  	if (elem.classList.contains(className)) {
+  	  matchElems.push(elem);
+  	  // console.log(matchElems);
+  		// return matchElems;
   	}
   }
-  else {
-  	// return array
-  	console.log('made it to else')
-  	console.log(matchElems);
-  }
-  
-  
 
-/*
-  if (elem.childNodes.length!==0) {
-  	// change this to 0 -- need to figure out how to get rid of #text
-    for (var i = 0; i < elem.childNodes.length; i++) {
-      console.log(elem.childNodes[i].classList.contains(className));
-   	  if (elem.childNodes[i].classList.contains(className)) {
-  		matchElems.push(elem.childNodes[i].classList);
-  	  }
-  	  if (elem.childNodes[i].childNodes.length!=0) {
-  	  	// should this be where I reset the value of elem ?
-  	  }
-  	  // or here ? .. probably not
-  	  elem = elem.childNodes[i];
-  	  console.log(elem);
-  	  //elem.getElementsByClassName(className);
-    } 
-  }
   else {
-  	// return array that is converted to html doc
-  } */
+  	// console.log('made it to else');
+  	// console.log(matchElems);
+  	return matchElems;
+  }
+
+  for (var i = 0; i < elem.childNodes.length; i++) {
+  	if (elem.childNodes.length !== 0) {
+  		var child = elem.childNodes[i];
+  		getElementsByClassName(className, child, matchElems);
+  	}
+  }
+  // return matchElems;
+
 };
-
-
-// need to append each child node to elem to continue going deeper
-// the childNodes.length will = 0 if there are no child nodes.
-// this is probably what should be used to stop the recursion.
-
-// this actually needs to be called later - once I get all of the decendants
-  	// if (elem.childNodes[i].classList.contains(className)) {
-  		//console.log(body.childNodes);
-  		//elemsArray.push(elem.childNodes[i]);
