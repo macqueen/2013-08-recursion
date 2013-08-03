d19013dc5dab468dbdb8362be3d9b50ded2d76e8
macqueen <jessmacqueen@gmail.com>
2013-08-02 20:04:04 -0700

    fixed matchElems/matches bug

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,29 +6,25 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className, currentNode, matches) {
   var elem = currentNode || document.body;
-  var matchElems = matchElems || [];
+  var matchElems = matches || [];
 
   if (elem.nodeType === 1) {
   	if (elem.classList.contains(className)) {
   	  matchElems.push(elem);
-  	  // console.log(matchElems);
   	  return matchElems;
   	}
   }
 
   else {
-  	// console.log('made it to else');
-  	// console.log(matchElems);
   	return matchElems;
   }
 
   for (var i = 0; i < elem.childNodes.length; i++) {
   	if (elem.childNodes.length !== 0) {
   	  var child = elem.childNodes[i];
-  	  console.log(child);
   	  getElementsByClassName(className, child, matchElems);
   	}
   }
-  // return matchElems;
+  return matchElems;
 
 };
